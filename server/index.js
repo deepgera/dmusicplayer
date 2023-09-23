321c11aa94daad5fe04b7bc8ec0482c1f5465e26
deepgera <deepakgera9112@gmail.com>
2023-09-23 16:15:23 +0530

    modified package to be  deployed

--- server/index.js
+++ server/index.js
@@ -3,18 +3,18 @@ const cors = require("cors");
 const ytdl = require("ytdl-core");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 ////////cors()
 
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, '../build')));
 ////////port open
 app.listen(port, () => {
   console.log(`Server Works !!! At port ${port}`);
 });
 
 /////get request handel here get url for one video
-
 app.get("/download", (req, res) => {
   var URL = req.query.URL;
 
@@ -25,4 +25,10 @@ app.get("/download", (req, res) => {
       res.json(format[1].url);
     })
     .catch((err) => console.log(err));
+});
+
+
+app.use('*',async (req,res)=>{
+  console.log(path.join(__dirname, '../build/', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build/', 'index.html'))
 });
